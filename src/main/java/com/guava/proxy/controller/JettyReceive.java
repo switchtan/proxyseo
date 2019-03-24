a765bb4f37cb3475492186d6c47ac6d1e4922609
guava <399852658@>
2019-03-24 23:14:12 +0800

    搞定css cache问题 还有html的cache问题 还有判断url的类别问题，url有几类呢 这个得先想清楚，另外就是要怎么过滤和加关键词的问题

--- src/main/java/com/guava/proxy/controller/JettyReceive.java
+++ src/main/java/com/guava/proxy/controller/JettyReceive.java
@@ -5,34 +5,67 @@ import com.guava.proxy.Util.HttpHelper;
 import com.guava.proxy.regex.GetCss;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.PersistentCacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
+import org.ehcache.config.units.MemoryUnit;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 @Component
 public class JettyReceive extends AbstractHandler {
+    private CacheManager cm=null;
+    private Cache<String, String> cssCache=null;
+    JettyReceive(){
+        if(cm==null) {
+            cm = this.getCacheManager();
+        }
+        cssCache = cm.getCache("cssCache", String.class, String.class);
 
+    }
     @Override
     public void handle(String url, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
+        String baseUrl="https://www.36kr.com/";
+
         boolean isReturned=false;
-        Pattern p = Pattern.compile("\\.css$");
-        Matcher m=p.matcher(url);
-        if(m.find()){
+        System.out.println("get a request2:"+url);
+
+//        Pattern p = Pattern.compile(".*\\.css");
+//        Matcher m=p.matcher(url);
+        //if(m.find()){
+
+        if(url.indexOf(".css")>-1){
+            System.out.println("found css:"+url);
+            response.setContentType("text/css;charset=UTF-8");
+            url=url.replace("/","");
+            if(cssCache.containsKey(url)){
+                response.getWriter().println(cssCache.get(url));
+            }else {
+                response.getWriter().println(".guava{}");
+            }
+            baseRequest.setHandled(true);
+            isReturned=true;
+
+        }
+        if(isReturned==false && url.indexOf(".ico")>-1){
             response.getWriter().println(".guava{}");
-            //response.flushBuffer();
             baseRequest.setHandled(true);
             isReturned=true;
         }
-
         if(isReturned==false) {
+            System.out.println("no css file");
+
             printHeader(url,request);
             //response.setContentType("application/json");
             response.setContentType("text/html;charset=UTF-8");
@@ -40,9 +73,11 @@ public class JettyReceive extends AbstractHandler {
             HttpHelper httpHelper = new HttpHelper();
             try {
                 String html = httpHelper.sentGet("https://www.36kr.com/");
-                GetCss getCss=new GetCss();
+                GetCss getCss=new GetCss(cssCache);
                 html=getCss.getC(html);
+
                 response.getWriter().println(html);
+
                 //response.flushBuffer();
                 baseRequest.setHandled(true);
             } catch (InterruptedException e) {
@@ -117,5 +152,20 @@ public class JettyReceive extends AbstractHandler {
         System.out.println("ip:" + getIpAdrress(request));
         //System.out.println("body:" + getBodyDate(request));
     }
+
+    private CacheManager getCacheManager(){
+        PersistentCacheManager persistentCacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                .with(CacheManagerBuilder.persistence(new File( "myData")))
+                .withCache("cssCache",
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, String.class,
+                                ResourcePoolsBuilder.newResourcePoolsBuilder()
+                                        .heap(10, EntryUnit.ENTRIES)
+                                        .offheap(1, MemoryUnit.MB)
+                                        .disk(20, MemoryUnit.MB, true)
+                        )
+                ).build(true);
+        return persistentCacheManager;
+    }
+
 
 }
